ff1a4d62f96ca101eb4d28b67fc4cf4717f4f23d
Ganjalo <jean.hopp@gmail.com>
2017-02-11 09:41:40 +0100

    Added Heroku landing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Tuiter"
+    # puts "<p>Ruby version: #{RUBY_VERSION}</p>"
+    # puts "<p>Rails version: #{Rails.version}</p>"
+  end
 end
