9bbaba141228af13a78683216643f060cbc42a97
Ganjalo <jean.hopp@gmail.com>
2017-02-13 22:48:27 +0100

    Added user edit view and controller, added form helper, updated signup form action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,13 @@ class UsersController < ApplicationController
     end
   end
 
-    private
-      def user_params
-        params.require(:user).permit(:name, :email, :password,
-                                     :password_confirmation)
-      end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
