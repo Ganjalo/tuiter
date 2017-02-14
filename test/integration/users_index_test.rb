9178b3f7dd0a8b6f8b2e1b0cfb01af2c2c0c6e20
Ganjalo <jean.hopp@gmail.com>
2017-02-14 06:07:24 +0100

    Added accound activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,7 +11,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination', count = 2
+    assert_select 'div.pagination', count: 2
     nonadmin_user = User.find_by(admin: false)
     assert_select 'a[href=?]', user_path(nonadmin_user), text: nonadmin_user.name
     assert_select 'a[href=?]', user_path(nonadmin_user), text: 'Delete'
@@ -26,4 +26,18 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count: 0
   end
 
+  test "index only shows validated users" do
+    log_in_as(@non_admin)
+    inactiveuser = User.find_by(activated: false)
+    get users_path
+    assert_select 'a[href=?]', user_path(inactiveuser),
+                   text: inactiveuser.name, count: 0
+  end
+
+  test "inactive user view redirects to root" do
+    log_in_as(@admin)
+    inactiveuser = User.find_by(activated: false)
+    get user_path(inactiveuser)
+    assert_redirected_to root_url
+  end
 end
