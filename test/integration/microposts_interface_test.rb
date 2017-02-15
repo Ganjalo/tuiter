1b275fd98c16174a5116034254ff6dd616380a49
Ganjalo <jean.hopp@gmail.com>
2017-02-15 17:37:08 +0100

    Added user microposts

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,27 +10,28 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Invalid submission
-    assert_no_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: "" } }
-    end
+    post microposts_path, micropost: { content: "" }
     assert_select 'div#error_explanation'
     # Valid submission
-    content = "This micropost really ties the room together"
+    content = "This micropost really ties the room together."
+    picture = fixture_file_upload('test/fixtures/files/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
-    assert_redirected_to root_url
+    assert assigns(:micropost).picture?
     follow_redirect!
     assert_match content, response.body
-    # Delete post
-    assert_select 'a', text: 'Delete'
+    assert_select ".content > img"
+    # Delete a post.
+    assert_select 'a', 'Delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
-    # Visit different user (no delete links)
+    # Visit a different user.
     get user_path(users(:archer))
-    assert_select 'a', text: 'Delete', count: 0
+    assert_select 'a', { text: 'Delete', count: 0 }
   end
 end
